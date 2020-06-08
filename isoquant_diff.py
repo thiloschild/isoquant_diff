d994fc9d1e9aa399ecd56d5dc879bb7837625e88
ThiloSchild <62449441+thiloschild@users.noreply.github.com>
2020-06-08 11:30:09 +0200

    argparse update

--- isoquant_diff.py
+++ isoquant_diff.py
@@ -3,6 +3,8 @@ import pandas as pd
 import xlrd
 import webbrowser
 import os.path
+import sys
+import argparse
 
 
 def get_data(file):
@@ -94,7 +96,7 @@ def create_list_of_parameters(dict1,dict2):
 	return parameters
 
 
-def diff_config(file1,file2,report_unique=True):
+def diff_config(file1,file2,csv,report_unique=True):
 
 
 	dict1 = get_data(file1)
@@ -118,14 +120,19 @@ def diff_config(file1,file2,report_unique=True):
 	
 	file1 = os.path.basename(file1) #change paths to filenames
 	file2 = os.path.basename(file2)
-	html = df.to_html()
-	df.to_excel("diff_between_"+file1+"_"+file2+".xlsx")
-	text_file = open("diff_between_"+file1+"_"+file2+".html", "w")
-	text_file.write(html)
-	text_file.close()
-	print("------------------")
-	print("A comparison file has been created!")
-	print("------------------")
+	if csv == True:
+		df.to_excel("diff_between_"+file1+"_"+file2+".xlsx")
+		print("------------------")
+		print("A comparison file (csv) has been created!")
+		print("------------------")
+	if csv == False:
+		html = df.to_html()
+		text_file = open("diff_between_"+file1+"_"+file2+".html", "w")
+		text_file.write(html)
+		text_file.close()	
+		print("------------------")
+		print("A comparison file (html) has been created!")
+		print("------------------")
 
 
 def conv2float(s):
@@ -150,9 +157,20 @@ def file_existing(file):
 	return x
 
 #########################################################################
-
-def main():
 	
+def main():
+
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-c", "--csv", action="store_true", required=False,
+					help="output as csv (default: html-file)")
+	ap.add_argument("-r", "--report_unique", action="store_false", required=False, 
+					help="compares only parameters which are defined in both config-files")
+	args = vars(ap.parse_args())
+
+	csv = args["csv"]
+	report_unique = args["report_unique"]
+
+
 	file1 = input("Enter the name or the path of the first file: ")
 
 	if validate_file(file1) == False:
@@ -184,7 +202,12 @@ def main():
 			print("------------------")
 	
 		else:
-			diff_config(file1,file2)
+			diff_config(file1,file2,csv,report_unique)
 			file1 = os.path.basename(file1)
 			file2 = os.path.basename(file2)
-			webbrowser.open("diff_between_"+file1+"_"+file2+".html", new=2)	
+			if csv == False:
+				webbrowser.open("diff_between_"+file1+"_"+file2+".html", new=2)
+
+
+if __name__ == "__main__":
+    main()
